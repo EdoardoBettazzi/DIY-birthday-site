3fb64f45c012b1d86beab225f18405d1dbf18d6c
EdoardoBettazzi <edoardobettazzi1@gmail.com>
2026-04-16 23:14:28 +0200

    minor

--- app.py
+++ app.py
@@ -365,7 +365,7 @@ st.markdown(f"""
     dalle da-da i su corpo che gli-gli
     leccano la schiena
     poi poi gli sputano e leccano i
-    groppone..
+    gropp..
   </div>
   <div class="ev-footer-main">— ci vediamo il 26 —</div>
 </div>
